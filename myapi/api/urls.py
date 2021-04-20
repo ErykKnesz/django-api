96c47e0c3d886e2745928841ab3c13395f518c98
Eryk Knesz <eryk.knesz@gmail.com>
2021-04-20 22:02:37 +0200

    Add json response to thumbnails from ImageDetail view

--- myapi/api/urls.py
+++ myapi/api/urls.py
@@ -3,8 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('images/', views.ImageList.as_view()),
-    path('images/<int:pk>/', views.ImageDetail.as_view()),
-    path('thumbnail200/<int:pk>/', views.thumbnail_200, name='thumbnail200'),
-    path('thumbnail400/<int:pk>/', views.thumbnail_400, name='thumbnail400'),
+    path('images/', views.ImageList.as_view(), name='image_list'),
+    path('images/<int:pk>/',
+         views.ImageDetail.as_view(),
+         name='image-details'),
+    path('images/<int:pk>/thumbnail200/',
+         views.thumbnail_200,
+         name='thumbnail200'),
+    path('images/<int:pk>/thumbnail400/',
+         views.thumbnail_400,
+         name='thumbnail400'),
 ]
--- myapi/api/views.py
+++ myapi/api/views.py
@@ -3,10 +3,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.shortcuts import render
-from .models import Image, User
+from .models import Image
 from .serializers import ImageSerializer
 from .permissions import IsOwner
 from .thumbnails import create_thumbnail_200, create_thumbnail_400
+from django.http import JsonResponse
 
 
 class ImageList(generics.ListCreateAPIView):
@@ -22,8 +23,10 @@ class ImageList(generics.ListCreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        res_content = serializer.data['name'] # TO BE AMENDED!
-        return Response(res_content, status=status.HTTP_201_CREATED, headers=headers)
+        response = Response(serializer.data,
+                            status=status.HTTP_201_CREATED,
+                            headers=headers)
+        return response
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -37,27 +40,33 @@ class ImageDetail(generics.RetrieveAPIView):
     def retrieve(self, *args, **kwargs):
         instance = self.get_object()
         user = self.request.user
-        link_t_200 = reverse('thumbnail200', args=[instance.id], request=self.request)
+        link_t_200 = reverse('thumbnail200',
+                             args=[instance.id],
+                             request=self.request)
+        response = {
+            'Thumbnail200': link_t_200
+        }
         if user.is_premium():
-            #t_400 = create_thumbnail_400(instance, self.request)
-            link_t_400 = reverse('thumbnail400',args=[instance.id], request=self.request)
-            return Response(link_t_400)
-        #serializer = self.get_serializer(t)
-        #serializer.name = instance.name
-        #serializer.owner = instance.owner
-        return Response(link_t_200)
+            link_t_400 = reverse('thumbnail400',
+                                 args=[instance.id],
+                                 request=self.request)
+            response['Thumbnail200'] = link_t_400
+            return JsonResponse(response)
+
+        return JsonResponse(response)
 
 
 def thumbnail_200(request, pk=id):
     instance = Image.objects.get(pk=pk)
     context = {
-            'thumbnails': create_thumbnail_200(instance)
-        }
-    return render(request,'thumbnail200.html', context=context)
+        'thumbnails': create_thumbnail_200(instance)
+    }
+    return render(request, 'thumbnail200.html', context=context)
 
 
 def thumbnail_400(request, pk=id):
+    instance = Image.objects.get(pk=pk)
     context = {
-        'thumbnails': create_thumbnail_400()
+        'thumbnails': create_thumbnail_400(instance)
     }
     return render(request, 'thumbnail400.html', context=context)
