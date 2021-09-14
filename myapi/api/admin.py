1cfc172c5366baa296258a841c1939937c4b241d
Eryk Knesz <eryk.knesz@gmail.com>
2021-09-15 00:23:40 +0200

    remove old code registering User in admin.py

--- myapi/api/admin.py
+++ myapi/api/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
 from .models import Image, Account
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
-
-'''
-fields = list(UserAdmin.fieldsets)
-fields[0] = (None, {'fields': ('username', 'password', 'user_type')})
-UserAdmin.fieldsets = tuple(fields)
-'''
 
 admin.site.register(Image)
 admin.site.register(Account)
--- myapi/api/validators.py
+++ myapi/api/validators.py
@@ -1,8 +1,9 @@
 from django.core.exceptions import ValidationError
+from django.conf import settings
 
 
 def image_format_validator(img):
-    if img.format.upper() not in ('JPG', 'JPEG', 'PNG'):
+    if img.format.upper() not in settings.ALLOWED_EXTENSIONS:
         raise ValidationError(
             "Only JPG/JPEG and PNG image extensions are allowed,"
             f" but {img.format.upper()} was provided."
